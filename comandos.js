b0c99e9d9be8c97faf6169e7cb3dab43d70c93ec
Kevin Daniel Fondevila Ricardo <kevinfondevila@gmail.com>
2020-05-11 03:13:21 -0500

    Nuevas Apis y comandos.js
    Agregue nuevas  apis de donde sacar memes, pero todos estan en ingles,
    y tambien arregle el desastre que habia en el codigo con los comandos
    agregando un file de comandos.js

---
+++ comandos.js
@@ -0,0 +1,90 @@
+const fetch = require('node-fetch');
+const {MessageAttachment} = require('discord.js');
+
+module.exports = {
+    comandos:{
+        '.meme': {
+            nombre:".meme",
+            descripcion:"Muestra memes normi aleatorio",
+            ejecutar:(comando)=>{
+                mostrarMemes(comando.channel,comando.content);
+            }
+        },
+        '.dank': {
+            nombre:".dank",
+            descripcion:"Muestra memes|gifts|videos mas fuertes",
+            ejecutar:(comando)=>{
+                mostrarMemes(comando.channel,comando.content);
+            }
+        },
+        '.moderate': {
+            nombre:".dank",
+            descripcion:"Muestra memes|gifts|videos mas moderados",
+            ejecutar:(comando)=>{
+                mostrarMemes(comando.channel,comando.content);
+            }
+        },
+        '.light': {
+            nombre:".dank",
+            descripcion:"Muestra memes|gifts|videos mas sueves",
+            ejecutar:(comando)=>{
+                mostrarMemes(comando.channel,comando.content);
+            }
+        },
+        '.sbubby': {
+            nombre:".dank",
+            descripcion:"Muestra memes|gifts|videos normi",
+            ejecutar:(comando)=>{
+                mostrarMemes(comando.channel,comando.content);
+            }
+        },
+        '.furry': {
+            nombre:".dank",
+            descripcion:"Muestra memes|gifts|videos normi",
+            ejecutar:(comando)=>{
+                mostrarMemes(comando.channel,comando.content);
+            }
+        },
+        '.cls': {
+            nombre:".cls",
+            descripcion:"Limpia todo el chat",
+            ejecutar:(comando)=>{
+                comando.channel.bulkDelete(100,false)
+                    .then(()=>comando.channel.send("mensajes eliminados correctamente"));
+            }
+        }
+    }
+}
+
+/**
+ * https://meme-api.glitch.me
+ *                           /dank
+ *                           /moderate
+ *                           /light
+ *                           /furry
+ *                           /sbubby
+ */
+
+async function mostrarMemes(channel,tipo){
+    if(channel.name==="memes"){
+        if(tipo===".meme"){
+            fetch("https://meme-api.herokuapp.com/gimme")
+            .then(res=>res.json())
+            .then(json=>channel.send(json.title,new MessageAttachment(json.url)))        
+            .catch(error=>console.error(error))                        
+        }else{
+            fetch("https://meme-api.glitch.me/"+tipo.substring(1))
+            .then(res=>res.json())
+            .then(json=>{
+                json.meme.endsWith("mp4")?channel.send(json.meme):channel.send(new MessageAttachment(json.meme))                
+                    .catch(error=>{
+                        console.error(error);
+                        channel.send("Ocurrio un error, intentalo de nuevo en unos segundos :worried:");                    
+                    })
+            })
+            .catch(error=>console.error(error))                        
+        }    
+    }else{
+        channel.send("Si estas intentando pedirme un meme, intentalo de nuevo en el canal de memes, ayuda a mantener el server libre de spam :upside_down:");
+    }
+}
--- index.js
+++ index.js
@@ -1,62 +1,12 @@
 const Discord = require('discord.js');
-const client = new Discord.Client();
 require('dotenv').config()
+const {comandos} = require('./comandos.js');
+
+const client = new Discord.Client();
+
+
+client.login(process.env.token)    
 const tw = require('twilio')(process.env.accountSid,process.env.authToken);
-const filter = (reaction,user)=>['▶️','⏸️','⏭️'].includes(reaction.emoji.name)&&(user.id!=client.user.id);
-
-
-
-const fb = require('fb');
-fb.setAccessToken(process.env.fbkey);
-                    
-var ultimo_meme = {};
-
-async function mostrarMemes(channel){
-    let guild = channel.guild;
-    let sigue = true;
-    fb.api(
-        "/417445035653004/posts/?fields=full_picture&limit=20",
-        function (response) {
-          if (response && !response.error) {            
-            response.data.map(async value=>{
-                if(sigue&&value&&value.full_picture){
-                    if(ultimo_meme[guild]===value.id){
-                        sigue = false;
-                    }
-                    else{
-                        await channel.send({
-                            files: [{
-                                attachment: value.full_picture                                             
-                            }]
-                        }) 
-                    }
-                }                         
-            });
-            ultimo_meme[guild]=response.data[0].id;
-            setTimeout(()=>{mostrarMemes(channel)},18000);
-          }else{
-              console.log(response.error);              
-          }
-        }
-    );
-}
-
-
-
-
-/*
-FB.api(
-  '/me',
-  'GET',
-  {"fields":"posts.limit(20){message,attachments.limit(1){target,subattachments,type,media,unshimmed_url}}"},
-  function(response) {
-      // Insert your code here
-  }
-);
-
-*/
-
-
 
 
 
@@ -65,6 +15,7 @@ client.on('guildCreate',(guild)=>{
     let musica =guild.channels.cache.find(channel=>channel.name=="musica");
     let memes =guild.channels.cache.find(channel=>channel.name=="memes");
     let parent = guild.channels.cache.find(channel=>channel.name=="general").parent;
+
     if(!musica){                                        
         guild.channels.create("musica",{type:"text",parent:parent,position:1})
             .then(channel=>channel.send("**LISTO PARA ESCUCHAR MUSICA SEMPAII!** \nhttps://tenor.com/view/listening-to-music-dancing-music-notes-enjoying-gif-14460643"));
@@ -74,26 +25,24 @@ client.on('guildCreate',(guild)=>{
     }
     if(!memes){
         guild.channels.create("memes",{type:"text",parent:parent,position:2})
-            .then(channel=>mostrarMemes(channel));                     
+        //    .then(channel=>mostrarMemes(channel,"dank"));                     
     }    
     else{
-        mostrarMemes(memes);
+        //mostrarMemes(memes,"dank");
     }    
 
 })
 
-
-client.once('ready', () => {    
-    console.log('Ready!');   
-
+client.once('ready', () => {        
+    console.log('Ready!');            
     client.guilds.cache.map(guild=>{
-         memes = guild.channels.cache.find(channel=>channel.name=="memes");
-         if(memes)
-            mostrarMemes(memes);
-    });
+        memes = guild.channels.cache.find(channel=>channel.name=="memes");               
+
+        })            
 });
 
 client.on('message',(message)=>{        
+    
 //////   CONTROL DEL CANAL DE MUSICA    ///////////////////////////////////////////////////////////////////////////    
     if(message.channel.name=="musica"){                
         if(!message.author.bot){           
@@ -110,7 +59,7 @@ client.on('message',(message)=>{
         })
         ms.setImage("https://i.imgur.com/4bn36.jpg");
         setTimeout(()=>{message.delete();},300);                       
-        message.channel.send(ms);
+        message.channel.send(ms);        
     }
     else if(message.author.bot){
         if(message.author==client.user){
@@ -138,32 +87,11 @@ client.on('message',(message)=>{
       .then(message => console.log(message.sid));
     }
     */
-    
+        
+    let mensajeText = message.content;
+    if(mensajeText.startsWith('.')){        
+        comandos[mensajeText]?comandos[mensajeText].ejecutar(message):message.channel.send("Ese comando no existe :confused:");
+    }
 
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////////    
-   /* else if(message.content===".console"&&message.channel.name=="musica"){                               
-        message.channel.send("**LISTO PARA ESCUCHAR MUSICA SEMPAII!** \nhttps://tenor.com/view/listening-to-music-dancing-music-notes-enjoying-gif-14460643").then(async message=>{                                                                                
-        })                                                                                            
-    .catch(error=>console.log(error.message))                            
-    }
-    */
-    
-    if(message.content===".cls"){       
-        message.channel.bulkDelete(100,false)
-            .then(()=>message.channel.send("mensajes eliminados correctamente"));
-        /*                
-        message.channel.messages.fetch()
-            .then(async collection=>{                                
-                 await collection.map(async message=>{
-                     if(message){
-                      await message.delete()                        
-                        .catch(error=>console.log(error))
-                    }
-                })                   
-            })
-            .then(canCls=true)                        
-            .catch(error=>{console.log(error);canCls=true;})    */
-    }
+
 });
-
-client.login(process.env.token);
